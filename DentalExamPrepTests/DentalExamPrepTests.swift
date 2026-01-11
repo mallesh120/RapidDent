b77c1a7b3a0228171353f5a069ba6efcbd0f6bf2
Sai Mallesh <mallesh120@gmail.com>
2026-01-10 20:26:43 -0500

    Initial Commit

---
+++ DentalExamPrepTests/DentalExamPrepTests.swift
@@ -0,0 +1,16 @@
+//
+//  DentalExamPrepTests.swift
+//  DentalExamPrepTests
+//
+//  Created by Mallesh on 1/10/26.
+//
+
+import Testing
+
+struct DentalExamPrepTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
