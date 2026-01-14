3eb35ba6e0d0f2f9e99754e92ffca86f575278bf
Sai Mallesh <mallesh120@gmail.com>
2026-01-13 23:11:04 -0500

    Initial Commit

---
+++ RapidDent/RapidDentApp.swift
@@ -0,0 +1,17 @@
+//
+//  RapidDentApp.swift
+//  RapidDent
+//
+//  Created by Mallesh on 1/13/26.
+//
+
+import SwiftUI
+
+@main
+struct RapidDentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ RapidDentTests/RapidDentTests.swift
@@ -0,0 +1,16 @@
+//
+//  RapidDentTests.swift
+//  RapidDentTests
+//
+//  Created by Mallesh on 1/13/26.
+//
+
+import Testing
+
+struct RapidDentTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
